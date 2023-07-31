275efbdfb21fdc16f4126bdd5a5ec312f8911bea
Lâm Diễm Kiều <kieulam6396@gmail.com>
2023-07-31 16:24:57 +0700

    'dayCode'

---
+++ src/Menu.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div className="multiple-items py-5 w-10/12 m-auto">
+      <div className="w-20 bg-green-400 text-center">Hồ bơi</div>
+      <div className="w-20 text-center">Bãi biển</div>
+      <div className="w-20 text-center">Nhà nhỏ</div>
+      <div className="w-20 text-center">Thiết kế</div>
+      <div className="w-20 text-center">bắc cực</div>
+      <div className="w-20 text-center">Cabin</div>
+    </div>
+  );
+};
+
+export default Menu;
---
+++ src/redux/giaoTiepAPI.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const giaoTiepAPI = () => {
+  return (
+    <div>giaoTiepAPI</div>
+  )
+}
+
+export default giaoTiepAPI
